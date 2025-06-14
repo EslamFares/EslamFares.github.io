b0e26ae287e3bc816c57ceb1f7b2401230d66eea
EslamFares <islamfareselazb@gmail.com>
2025-06-14 07:03:16 +0300

    ver 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1825331341"
+    serviceWorkerVersion: "232193931"
   }
 });
